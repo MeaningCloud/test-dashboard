f9327c43ca960a373d13f6e6115f77e8e107c966
Gasan Nazer <gnazer@meaningcloud.com>
2020-09-24 10:41:20 +0200

    changes in renderer

--- report_renderer.js
+++ report_renderer.js
@@ -1,55 +1,35 @@
 "using strict"
 
-let count = 0;
-
-function myFunction() {
+function myFunction(reportsList, path) {
     let reportDiv = $("#reports")
-    reportDiv.append("<p> HELLO </p>")
-
-    
-/*
-    <ul>
-    </ul>
-
-    let startYear = $("#startYear option:selected").text();
-    let endYear = $("#endYear option:selected").text();
-    if(startYear > endYear){
-        let a = startYear
-        startYear = endYear
-        endYear = a
+    reportDiv.append("<ul>")
+    for(element of reportsList){
+        console.log(element)
+        console.log(element.path.replace(":", "%3A"))
+        reportDiv.append(`<li> <a href="${path}/${element.path.replace(":", "%3A")}/index.html">${element.path} </a></li>`)
     }
-    let imageName = element + "_" + startYear + "_" + endYear + ".png";
-    console.log(imageName);
-
-    clear();
-    let imageParent = document.getElementById("body");
-    let image = document.createElement("img");
-    image.id = "id";
-    image.className = "classImg";
-    image.src = "model/combine/diagrams/" + imageName; // image.src = "IMAGE URL/PATH"
-    imageParent.appendChild(image);
-*/
-}
-
-function clear() {
-    document.getElementById("body").innerHTML = "";
+    reportDiv.append("</ul>")
 }
 
 $(document).ready(function() {
-    myFunction();
     let path = window.location.pathname;
     path = path.substring(0, path.lastIndexOf('/'));
-    console.log(path)
 
     let reportsPath = path + "/reports"
 
-    console.log(reportsPath)
-
-
-    var fileNames = new Array();
     $.ajax({
         url: "https://api.github.com/repos/MeaningCloud/test-dashboard/git/trees/master",
     }).done(function( data ) {
-        console.log( "Sample of data:", data);
+        let reportsInfo = null;
+        for (element of data.tree){
+            if(element.path === "reports")
+                reportsInfo = element;
+        }
+
+        $.ajax({
+            url: reportsInfo.url,
+        }).done(function( data ) {
+            myFunction(data.tree, reportsPath)
+        });
     });
 });
