009d6940ac8c097b1a8bc432f3329e357330c518
GasanNazer <40351273+GasanNazer@users.noreply.github.com>
2020-09-24 10:00:47 +0200

    Update report_renderer.js

--- report_renderer.js
+++ report_renderer.js
@@ -48,38 +48,11 @@ $(document).ready(function() {
 
     var fileNames = new Array();
     $.ajax({
-      url: "reports",
-      success: function(data){
-            print(data)
-
-        /* $(data).find("td > a").each(function(){
-            if(openFile($(this).attr("href"))){
-                fileNames.push($(this).attr("href"));
-            }           
-         });*/
-      }
-    }); 
-    /*
-    console.log(fileNames);
-    function openFile(file) {
-        var extension = file.substr( (file.lastIndexOf('.') +1) );
-        switch(extension) {
-            case 'jpg':
-            case 'png':
-            case 'gif':   // the alert ended with pdf instead of gif.
-            case 'zip':
-            case 'rar':
-            case 'pdf':
-            case 'php':
-            case 'doc':
-            case 'docx':
-            case 'xls':
-            case 'xlsx':
-                return true;
-                break;
-            default:
-                return false;
+        url: "https://meaningcloud.github.io/test-dashboard/reports", 
+       
+    }).done(function( data ) {
+        if ( console && console.log ) {
+            console.log( "Sample of data:", data);
         }
-    };
-    */
+    });
 });
