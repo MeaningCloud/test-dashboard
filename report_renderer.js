442b90de23cad01eed606dccd049ccb433cca50d
Gasan Nazer <gnazer@meaningcloud.com>
2020-09-24 11:51:35 +0200

    cleaning and ordering report by date of publish desc

--- report_renderer.js
+++ report_renderer.js
@@ -2,11 +2,30 @@
 
 function myFunction(reportsList, path) {
     let reportDiv = $("#reports")
-    reportDiv.append("<ul>")
+    
+    reportsSorted = []
     for(element of reportsList){
-        console.log(element)
-        console.log(element.path.replace(":", "%3A"))
-        reportDiv.append(`<li> <a href="${path}/${element.path.replace(":", "%3A")}/index.html">${element.path} </a></li>`)
+        elementSplit = element.path.split("-")
+        timeSplit = elementSplit[1].split(":")
+        date = new Date(elementSplit[4], parseInt(elementSplit[3]) - 1, elementSplit[2], parseInt(timeSplit[0]) + 2, timeSplit[1])
+        elementPath = element.path
+        let obj = {date: date,
+                   path: elementPath
+                }
+        reportsSorted.push(obj);
+    }
+
+    reportsSorted.sort((a, b) => {
+        return b.date - a.date;
+    })
+
+    // create unordered list
+    reportDiv.append("<ul>")
+    for(element of reportsSorted){
+        elementSplit = element.path.split("-")
+        timeSplit = elementSplit[1].split(":")
+
+        reportDiv.append(`<li> <a href="${path}/${element.path.replace(":", "%3A")}/index.html">${elementSplit[0]}-${parseInt(timeSplit[0]) + 2}:${timeSplit[1]}-${elementSplit[2]}-${elementSplit[3]}-${elementSplit[4]}</a></li>`)
     }
     reportDiv.append("</ul>")
 }
